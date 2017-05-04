a0de14d85e9537e2abb11fb6fc9e50658251fb01
miha500 <miha.slatnar@gmail.com>
2017-05-04 12:47:43 +0200

    Miha je car

--- database.php
+++ database.php
@@ -4,7 +4,7 @@ $username = 'root';
 $password = '';
 $database = 'turistika';
 $server = 'localhost';
-//povezali smo se na baz!!!o
+//povezali smo se na baz
 $link = mysqli_connect($server, $username, $password, $database);
 //težava php in utf-8
 mysqli_query($link, "SET NAMES 'utf8'");
