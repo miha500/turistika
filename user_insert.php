a09286b7469b7d8b0bcf1ab044ef2300a2e12bff
StropnikBoy <thephonex23@gmail.com>
2017-05-18 13:44:04 +0200

    agencija

--- user_insert.php
+++ user_insert.php
@@ -6,25 +6,53 @@
     $email = $_POST['email'];
     $pass = $_POST['pass'];
     $pass2 = $_POST['pass2'];
+    $status = $_POST['status'];
     
-    if (!empty($last_name) && !empty($email) && !empty($pass)
-            && ($pass == $pass2)) {
-        //vse ok
-        $pass = $salt.$pass; //geslu dodam salt
-        $pass = sha1($pass); //zakodiram geslo
-        $query = sprintf("INSERT INTO users (first_name, last_name, email, pass) 
+    if ($status == "user") 
+        {
+        if (!empty($last_name) && !empty($email) && !empty($pass)
+                && ($pass == $pass2)) {
+            //vse ok
+            $pass = $salt.$pass; //geslu dodam salt
+            $pass = sha1($pass); //zakodiram geslo
+            $query = sprintf("INSERT INTO users (first_name, last_name, email, pass) 
                           VALUES ('%s','%s','%s','$pass')",
                         mysqli_real_escape_string($link, $first_name),
                         mysqli_real_escape_string($link, $last_name),
                         mysqli_real_escape_string($link, $email));
-        mysqli_query($link, $query);
+            mysqli_query($link, $query);
+        
         
         //preusmeritev na prijavo
         header("Location: login.php");
-    }
-    else {
-        //neustrezni podatki
-        header("Location: registration.php");
-    }   
+        }
+        else {
+            //neustrezni podatki
+            header("Location: registration.php");
+             }   
+       }  
     
+    else
+        {
+        if (!empty($last_name) && !empty($email) && !empty($pass)
+                && ($pass == $pass2)) {
+            //vse ok
+            $pass = $salt.$pass; //geslu dodam salt
+            $pass = sha1($pass); //zakodiram geslo
+            $query = sprintf("INSERT INTO users (first_name, last_name, email, pass) 
+                          VALUES ('%s','%s','%s','$pass')",
+                        mysqli_real_escape_string($link, $first_name),
+                        mysqli_real_escape_string($link, $last_name),
+                        mysqli_real_escape_string($link, $email));
+            mysqli_query($link, $query);
+        
+        
+        //preusmeritev na prijavo
+        header("Location: login.php");
+        }
+        else {
+            //neustrezni podatki
+            header("Location: registration.php");
+             }   
+        }
 ?>
