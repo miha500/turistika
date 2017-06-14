b35e8b7cb335b27f51352e43aee23df2fbf2bb4b
miha500 <miha.slatnar@gmail.com>
2017-06-14 10:43:43 +0200

    agency_comments

--- agency.php
+++ agency.php
@@ -2,7 +2,23 @@
 
 include_once 'header.php';
 include_once 'database.php';
+
+$agency_id = $_GET['id'];
+
+$query = "SELECT * FROM agencies WHERE id = $agency_id";
+$result = mysqli_query($link, $query);
+$agency = mysqli_fetch_array($result);
+    
 ?>
+
+<p><?php echo $agency['title'];?></p>
+<form method="post" action="agency_comment_add.php">
+    <input type="hidden" name="agency_id"
+               value="<?php echo $agency_id; ?>" />
+    <textarea rows="5" cols="5" name="comment"></textarea>
+    <input type="submit" name="submit">
+    
+</form>
 
 <?php 
 include_once 'footer.php';
